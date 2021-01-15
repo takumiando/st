9bfbb8e2e48eeeca592c2a75ea94bdda265d37ce
Takumi Ando <takumi@t15.ch>
2021-01-15 11:32:21 +0900

    Modify color palette to monochrome
    Signed-off-by: Takumi Ando <takumi@t15.ch>

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Inconsolata:pixelsize=14:antialias=true:autohint=true";
+static int borderpx = 20;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -96,24 +96,26 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#000000",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
 
 	[255] = 0,
 
